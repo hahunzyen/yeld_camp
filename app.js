14b4108fb4d18b89a4229109d2a0e1606ec61632
Kevin <tangkien@Tangs-MacBook-Pro.local>
2020-05-27 17:11:34 +0700

    add

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 //=============
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("SEVER HAS STARTED");
 })
